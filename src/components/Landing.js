3ecfe93b246e6e479fffc68936e9ef902d440eb7
mattiesansev <mesansev@gmail.com>
2020-09-16 17:32:15 -0700

    simple landing

---
+++ src/components/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+
+function Landing() {
+    return (
+    <div>
+        <a
+          className="Title"
+        >
+          Uncharted Waters
+        </a>
+    </div>
+    )
+}
+
+export default Landing
