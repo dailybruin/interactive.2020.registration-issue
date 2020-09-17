a706528e85ab1c85452944a6f2baa9d50116630c
mattiesansev <mesansev@gmail.com>
2020-09-16 17:57:16 -0700

    landing with all components and text

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,6 +4,21 @@ import '../App.css';
 function Landing() {
     return (
     <div>
+        <div
+        className="InfoBox"
+        >
+            Registration Issue
+        </div>
+        <div
+        className="TwentyTwenty"
+        >
+            2020
+        </div>
+        <div
+        className="ByDB"
+        >
+            by the Daily Bruin
+        </div>
         <a
           className="Title"
         >
