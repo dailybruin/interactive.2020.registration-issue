961807bd9289d01c7ce351203c715e477e0a426d
Neil Prajapati <neilp0101@gmail.com>
2020-09-19 16:44:02 -0700

    Put inside Section component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Landing from "./components/Landing.js"
-import ArticleLine from "./components/ArticleLine"
+import Section from "./components/Section"
 
 
 function App() {
@@ -14,58 +14,7 @@ function App() {
       <p>
         hello here's some stuff under the landing
       </p>
-      <ArticleLine objects={[
-          {
-            "type": "article", 
-            "img": "https://wp.dailybruin.com/images/2020/09/fires.KM_1.jpg",
-            "author": "Elizabeth Sherwood",
-            "title": "Air quality in Westwood is unhealthy",
-            "link": "https://dailybruin.com/2020/09/12/air-quality-in-westwood-deemed-unhealthy-as-a-result-of-nearby-fires"
-          },
-          {
-            "type": "article", 
-            "img": "https://wp.dailybruin.com/images/2020/09/fires.KM_1.jpg",
-            "author": "Elizabeth Sherwood",
-            "title": "Air quality in Westwood is unhealthy",
-            "link": "https://dailybruin.com/2020/09/12/air-quality-in-westwood-deemed-unhealthy-as-a-result-of-nearby-fires"
-          },
-          {
-            "type": "article", 
-            "img": "https://wp.dailybruin.com/images/2020/09/fires.KM_1.jpg",
-            "author": "Elizabeth Sherwood",
-            "description": "Air quality in Westwood is unhealthy because of wildfires east of Los Angeles.",
-            "link": "https://dailybruin.com/2020/09/12/air-quality-in-westwood-deemed-unhealthy-as-a-result-of-nearby-fires"
-          },
-          {
-            "type": "article", 
-            "img": "https://wp.dailybruin.com/images/2020/09/fires.KM_1.jpg",
-            "author": "Elizabeth Sherwood",
-            "description": "Air quality in Westwood is unhealthy because of wildfires east of Los Angeles.",
-            "link": "https://dailybruin.com/2020/09/12/air-quality-in-westwood-deemed-unhealthy-as-a-result-of-nearby-fires"
-          },
-          {
-            "type": "article", 
-            "img": "https://wp.dailybruin.com/images/2020/09/fires.KM_1.jpg",
-            "author": "Elizabeth Sherwood",
-            "title": "Air quality in Westwood is unhealthy because of wildfires east of Los Angeles.",
-            "link": "https://dailybruin.com/2020/09/12/air-quality-in-westwood-deemed-unhealthy-as-a-result-of-nearby-fires"
-          },
-          {
-            "type": "article", 
-            "img": "https://wp.dailybruin.com/images/2020/09/fires.KM_1.jpg",
-            "author": "Elizabeth Sherwood",
-            "description": "Air quality in Westwood is unhealthy because of wildfires east of Los Angeles.",
-            "link": "https://dailybruin.com/2020/09/12/air-quality-in-westwood-deemed-unhealthy-as-a-result-of-nearby-fires"
-          },
-          {
-            "type": "article", 
-            "img": "https://wp.dailybruin.com/images/2020/09/fires.KM_1.jpg",
-            "author": "Elizabeth Sherwood",
-            "description": "Air quality in Westwood is unhealthy because of wildfires east of Los Angeles.",
-            "link": "https://dailybruin.com/2020/09/12/air-quality-in-westwood-deemed-unhealthy-as-a-result-of-nearby-fires"
-          },
-
-        ]}/>
+      <Section/>
     </div>
   );
 }
