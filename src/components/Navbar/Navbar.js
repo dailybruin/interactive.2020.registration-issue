568c8401f608ed0e1133cdb5911bf510854d9df8
Sarthak More <ssmore12@gmail.com>
2020-09-23 13:22:03 -0700

    navbar without links done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,23 +1,74 @@
 import React from 'react';
 import '../../App.css';
+import styled from 'styled-components';
+
+const Title = styled('div')`
+    font-family: Source Sans Pro;
+    font-weight: 700;
+    font-size: 28px;
+    color: #fff;
+    @media (max-width: 768px) {
+        font-size: 36px;
+    }
+`
+
+const NavbarItems = styled('div')`
+    font-family: Source Sans Pro;
+    font-weight: 400;
+    font-size: 28px;
+    color: #fff;
+    display: flex;
+    flex-direction: row;
+    @media (max-width: 768px) {
+        width: 100%;
+        font-size: 24px;
+        justify-content: space-evenly;
+    }
+`
+
+const NavbarItem = styled('div')`
+    margin-left: 35px;
+    @media (max-width: 768px) {
+        margin-left: 0;
+    }
+`
+
+const Bar = styled('div')`
+    height: 75px; 
+    position: -webkit-sticky;
+    top: 0;
+    position: sticky;
+    background-color: #588BB8;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 36px;
+    z-index: 999;
+    @media (max-width: 768px) {
+        height: 100px;
+        flex-direction: column;
+        justify-content: center;
+    }
+`
 
 class Navbar extends React.Component {
     render() {
         return(
-            <div className='navbar'>
-                <div className='title'>registration issue 2020</div>
-                <div className='navbar_items'>
-                    <div className='navbar_item'>
+            <Bar>
+                <Title>registration issue 2020</Title>
+                <NavbarItems>
+                    <NavbarItem>
                         introduction
-                    </div>
-                    <div className='navbar_item'>
+                    </NavbarItem>
+                    <NavbarItem>
                         stories
-                    </div>
-                    <div className='navbar_item'>
+                    </NavbarItem>
+                    <NavbarItem>
                         about
-                    </div>
-                </div>
-            </div>
+                    </NavbarItem>
+                </NavbarItems>
+            </Bar>
         )
     }
 }
