36df80f61d0e1ebd1bfc9f808f55d5c83a55e87d
Neil Prajapati <neilp0101@gmail.com>
2020-09-26 16:19:39 -0700

    Added github pages build

---
+++ docs/build/precache-manifest.8100720e18f4127d0765887707230dec.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669506442c80ebd7293f5537bd6b8066",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df9706e1cecde3d88552",
+    "url": "/static/css/main.4afa2f9e.chunk.css"
+  },
+  {
+    "revision": "4a9ef355111076ab79fb",
+    "url": "/static/js/2.084e8a21.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.084e8a21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df9706e1cecde3d88552",
+    "url": "/static/js/main.8e427468.chunk.js"
+  },
+  {
+    "revision": "9d48a4a49f7e44d2daf9",
+    "url": "/static/js/runtime-main.7f84880d.js"
+  },
+  {
+    "revision": "019e032235747fd505226aea1908234f",
+    "url": "/static/media/ClearfaceBlackSSiBlack.019e0322.ttf"
+  },
+  {
+    "revision": "08a886361bf0012fbae607bbbf85889f",
+    "url": "/static/media/arrow.08a88636.svg"
+  },
+  {
+    "revision": "a439902c32bae8d52ea0018811244390",
+    "url": "/static/media/db.a439902c.png"
+  },
+  {
+    "revision": "e7b973c5193de6df7013d886d195f605",
+    "url": "/static/media/landing_background.e7b973c5.png"
+  }
+]);
