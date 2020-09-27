ad2d5eadeaf93a47d47c540a76749fe5f52e4812
Sarthak More <ssmore12@gmail.com>
2020-09-27 13:17:15 -0700

    fixed banner

--- src/components/BannerAd/index.js
+++ src/components/BannerAd/index.js
@@ -15,7 +15,7 @@ export default function BannerAd(props) {
 
 	switch(props.section){
 		case "HEADER":
-			return <a className="banner-ad" href={`https://ads.studentmedia.ucla.edu/www/delivery/ck.php?n=a322f0ab&amp;cb=INSERT_RA NDOM_NUMBER_HERE`} target='_blank'>
+			return <a className="banner-ad" href={`https://ads.studentmedia.ucla.edu/www/delivery/ck.php?n=a322f0ab&amp;cb=${rand}`} target='_blank'>
 				<img src={`https://ads.studentmedia.ucla.edu/www/delivery/avw.php?zoneid=81&amp;cb=${rand}&amp;n=abdc3f10`} border='0' alt='' /></a>;
 		case "OPINION":
 			return <a className="banner-ad" href={`http://ads.studentmedia.ucla.edu/www/delivery/ck.php?n=a322f0ab&amp;cb=${rand}`} target='_blank'>
